0009e47c9afd4c3f601021e1e390754e085e9870
Janell Huyck <janell.huyck@outlook.com>
2025-06-11 20:06:19 -0400

    Task 1 Complete

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,46 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// require scanner
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //define variables
+        double price;
+        double shippingCost;
+        double totalPrice;
+        final double SHIPPING_RATE = 0.02;
+        final double FREE_SHIPPING_CUTOFF = 100;
+        String trash;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // create a scanner in to read from the console
+        Scanner in = new Scanner(System.in);
+
+        // input the price of the item
+        System.out.print("Enter the price of the item: ");
+
+        if (in.hasNextDouble()) {
+            price = in.nextDouble();
+            in.nextLine();
+        } else {
+            trash = in.nextLine();
+            System.out.println( "Invalid input " + trash + ". Please enter a valid number.");
+            return;
         }
+
+        // determine if the price is $100 or more
+        if (price >= FREE_SHIPPING_CUTOFF) {
+            // when the price is equal to or greater than $100, the shippping cost is 0
+            shippingCost = 0;
+        } else {
+            // when the price is less than $100, shipping cost is 2% of price
+            shippingCost = price * SHIPPING_RATE;
+        }
+
+        // calculate totalPrice
+        totalPrice = price + shippingCost;
+
+        // output the shipping cost and total price
+        System.out.println("Item price: $" + price);
+        System.out.println("Shipping cost: $" + shippingCost);
+        System.out.println("Total price: $" + totalPrice);
     }
 }
